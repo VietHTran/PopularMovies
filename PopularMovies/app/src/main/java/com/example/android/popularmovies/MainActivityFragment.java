4bcaf246bc9be54cfd27c9fbf4638d7cbad7260f
VietHTran <thanos.trn11@gmail.com>
2016-10-05 12:14:29 -0500

    Add Refresh Button

--- PopularMovies/app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
+++ PopularMovies/app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.util.JsonWriter;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -53,22 +56,36 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
 
     }
+
+    //Setup the menu call by setHasOptionsMenu(true);
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        super.onCreateOptionsMenu(menu,menuInflater);
+        menuInflater.inflate(R.menu.menu_main_fragment,menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if (id==R.id.action_refresh) {
+            updateMovies();
+            return true;
+        }
+        return true;
+    }
+    //Save downloaded data in case of rotation
     @Override
     public void onSaveInstanceState(Bundle bundle) {
         bundle.putParcelableArrayList(POSTERS_KEY,posterList);
-        //Log.v("Test","locknload0");
         super.onSaveInstanceState(bundle);
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         //Load saved bundle to ArrayList
-        //Log.v("Test","locknload");
         if (savedInstanceState==null || !savedInstanceState.containsKey(POSTERS_KEY)) {
-            //Log.v("Test","locknload1");
             posterList= new ArrayList<Poster>();
         } else {
-            //Log.v("Test","locknload2");
             posterList=savedInstanceState.getParcelableArrayList(POSTERS_KEY);
         }
     }
@@ -76,11 +93,8 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-        //Log.v("Test","loadafterlock2.25");
         mPosterAdapter= new PosterAdapter(getActivity(), posterList);
-        //Log.v("Test","loadafterlock2.5");
         if (savedInstanceState==null || !savedInstanceState.containsKey(POSTERS_KEY)) {
-            //Log.v("Test","loadafterlock3");
             updateMovies();
         }
         GridView gridView = (GridView) root.findViewById(R.id.gridview_poster);
@@ -128,7 +142,7 @@ public class MainActivityFragment extends Fragment {
             // Will contain the raw JSON response as a string.
             String moviesJsonStr = null;
             //Please paste your API Key to the constant below
-            final String API_KEY="72f7940738a9f58a23116128df8550be";
+            final String API_KEY="";
             final String API_KEY_LABEL="api_key";
             try {
                 //Implement preference for sortByLater
@@ -184,11 +198,20 @@ public class MainActivityFragment extends Fragment {
         @Override
         protected void onPostExecute(Poster[] posters) {
             if (posters!=null) {
-                posterList.clear();
-                for (int i=0;i<posters.length;i++) {
-                    posterList.add(posters[i]);
+                //this is the first time
+                if (posterList.size()==0) {
+                    posterList.clear();
+                    for (int i=0;i<posters.length;i++) {
+                        posterList.add(posters[i]);
+                    }
+                } else
+                //this is the n>1 time
+                {
+                    mPosterAdapter.clear();
+                    for (int i=0;i<posters.length;i++) {
+                        mPosterAdapter.add(posters[i]);
+                    }
                 }
-                //mPosterAdapter= new PosterAdapter(getActivity(), posterList);
             }
         }
     }
