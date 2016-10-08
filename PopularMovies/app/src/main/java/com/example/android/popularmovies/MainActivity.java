2eb01f435df2fdbc7ffff84ba740e8ce2642e6c0
VietHTran <thanos.trn11@gmail.com>
2016-10-07 21:17:06 -0500

    Clean Project v2

--- PopularMovies/app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ PopularMovies/app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     @Override
@@ -31,9 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
